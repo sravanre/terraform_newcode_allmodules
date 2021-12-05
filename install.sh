f4d50091dad20b149a50276165109db810b28bb7
Your Name <you@example.com>
2021-12-05 22:49:33 +0530

    new

--- install.sh
+++ install.sh
@@ -3,4 +3,6 @@ sudo yum update
 sudo yum install -y httpd
 sudo chkconfig httpd on
 sudo service httpd start
-echo "<h1>Deployed via Terraform wih ELB</h1>" | sudo tee /var/www/html/index.html
+echo "<h1>Deployed via Terraform - SRAVAN  wih ELB</h1>" | sudo tee /var/www/html/index.html
+
+
