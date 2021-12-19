e56e64f20f280f6938bac4b719dedaac9d7795c9
Your Name <you@example.com>
2021-12-19 11:32:32 +0530

    vars

--- install.sh
+++ install.sh
@@ -4,3 +4,7 @@ sudo yum install -y httpd
 sudo chkconfig httpd on
 sudo service httpd start
 echo "<h1>Deployed via Terraform wih ELB</h1>" | sudo tee /var/www/html/index.html
+echo "<h1>Deployed via $(hostname -f)</h1>" | sudo tee /var/www/html/index.html
+echo "<h1>Deployed via $(hostname -i)</h1>" | sudo tee /var/www/html/index.html
+
+echo "<h1> Hello sravan how are you doing </h1>" | sudo tee /var/www/html/index.html
